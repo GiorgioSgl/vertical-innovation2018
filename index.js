295fc8038a76af2c8b848e16397d59cd0a130d79
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2018-11-17 03:29:51 +0100

    Revert "React"

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
 const weather = require('./weather');
 const geocoding = require('./graphhopper_geocoding');
 const routing = require('./graphhopper_routing');
-const points = require('./points');
+const express = require('express');
 
 const app = express();
-const jsonParser = bodyParser.json();
 const port = 8000;
 
 app.get('/', (req, res) => {
     res.send('Helloooo');
-});
-
-app.post('/points', jsonParser, async (req, res) => {
-    const location = req.body.location;
-
-    // const traffic; // TODO: invent random traffic
-    // const timeLastTime; // TODO: get time from database
-
-    let info = await weather.getWeatherInfo(location);
-
-    // const p = location.precipitation;
-    // const t = location.temperature;
-    // const p = parseFloat(location.precipitation);
-    // const t = parseFloat(location.temperature);
-
-    console.log(points.calculatePoints());
-
-    res.send(info);
 });
 
 app.listen(port, () => { console.log(`Back&Forth listening on port ${port}`); });
